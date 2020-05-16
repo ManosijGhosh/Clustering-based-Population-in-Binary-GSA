f772eca1303500b5946b9c8d586995bbf8c85bb2
RITAM GUHA <ritamguha16@gmail.com>
2020-05-16 11:46:36 +0530

    added comments

--- classify.m
+++ classify.m
@@ -1,13 +1,12 @@
 function [per]=classify(train,trainLabel,test,testLabel,agent,classifierType,paramValue)
+    % function to call the correct classifier depending on the type
     switch classifierType
         case 'mlp'
             [per]=mlpClassifier(train,trainLabel,test,testLabel,agent,paramValue);
         case 'svm'
             [per]=svmClassifier(train,trainLabel,test,testLabel,agent,paramValue);
         case 'knn'
-            [per]=knnClassifier(train,trainLabel,test,testLabel,agent,paramValue);
-        case 'nb'
-            [per]=nbClassifier(train,trainLabel,test,testLabel,agent,paramValue);
+            [per]=knnClassifier(train,trainLabel,test,testLabel,agent,paramValue);       
         otherwise
             warning('classifier type not present!!!!');
     end
